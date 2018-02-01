2ba49312aacc346eea1ff8e75672b445ee0fedf6
Colearo <colearolu@icloud.com>
2018-02-01 20:35:17 +0800

    update README
    Signed-off-by: Colearo <colearolu@icloud.com>

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='HuhuSeg',   
 
-    version='0.2.12',  
+    version='0.2.13',  
 
     description='Simple Chinese segmentator, keywords extractor and other examples',  
 
